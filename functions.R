1f1cd203f8049d7dd1439cc699b6bab4b5e99461
KZARCA <kevin.zarca@gmail.com>
2017-05-11 14:42:03 +0200

    add copyright

--- functions.R
+++ functions.R
@@ -1,7 +1,14 @@
-# seq_int <- function(from, to){
-#   if (to > 0) 
-#     seq.
-# }
+#
+# functions.R
+#
+# Copyright (C) 2016-17 Kevin Zarca
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
 
 paste_ <- function(...){
   paste(..., sep = "_")
